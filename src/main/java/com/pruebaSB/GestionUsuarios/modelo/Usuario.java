0c94ec1b120bdef8c544dd382b110a1b36be0f5a
AlainGZ <devalgz955@gmail.com>
2025-03-26 13:26:20 -0500

    Se actualiza la entidad Usuario

--- src/main/java/com/pruebaSB/GestionUsuarios/modelo/Usuario.java
+++ src/main/java/com/pruebaSB/GestionUsuarios/modelo/Usuario.java
@@ -1,4 +1,24 @@
 package com.pruebaSB.GestionUsuarios.modelo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name="usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	private String email;
+
 }
